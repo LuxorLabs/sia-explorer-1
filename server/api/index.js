d6aa28ee2b152cf89d8578bca42c06722fa4ea73
Nick <nitronick600@gmail.com>
2017-10-09 22:29:58 -0700

    Add caching and force https

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,8 @@
 const axios = require('axios')
 const router = require('express').Router()
 const CoinMarketCap = require('coinmarketcap-api')
+const NodeCache = require('node-cache')
+const cache = new NodeCache({ stdTTL: 15, checkperiod: 120 })
 
 const luxor = axios.create({
   baseURL: 'http://api.luxor.tech:8082',
@@ -9,38 +11,105 @@ const luxor = axios.create({
 
 const cmc = new CoinMarketCap()
 
+const STATS_KEY = 'StatsKey'
+const TICKER_KEY = 'Ticker'
+
 router.get('/stats', (req, res) => {
-  luxor
-    .get('/api/stats')
-    .then(({ data }) => {
-      res.send(data)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+  cache.get(STATS_KEY, (err, val) => {
+    if (!err) {
+      if (val === undefined) {
+        //cache miss
+        luxor
+          .get('/api/stats')
+          .then(({ data }) => {
+            cache.set(STATS_KEY, data)
+            res.send(data)
+          }).catch(err => {
+            console.log(err)
+          })
+      } else {
+        //cache hit (woo!)
+        res.send(val)
+      }
+    } else {
+      console.log('Error in caching stats infrastructure.  This needs to be investigated. Making the API call as a fallback')
+      luxor
+        .get('/api/stats')
+        .then(({ data }) => {
+          cache.set(STATS_KEY, data)
+          res.send(data)
+        }).catch(err => {
+          console.log(err)
+        })
+    }
+  })
 })
 
 router.get('/user/:address', (req, res) => {
   const address = req.params.address
-  luxor
-    .get(`/api/user/${address}`)
-    .then(({ data }) => {
-      res.send(data)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+  cache.get(address, (err, val) => {
+    if (!err) {
+      if (val === undefined) {
+        //cache miss
+        luxor
+          .get(`/api/user/${address}`)
+          .then(({ data }) => {
+            cache.set(address, data)
+            res.send(data)
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      } else {
+        //cache hit. 
+        res.send(val)
+      }
+    } else {
+      console.log('Error in caching address infrastructure.  This needs to be investigated. Making the API call as a fallback')
+      luxor
+        .get(`/api/user/${address}`)
+        .then(({ data }) => {
+          cache.set(address, data)
+          res.send(data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  })
 })
 
 router.get('/price', (req, res) => {
-  cmc
-    .getTicker({
-      currency: 'siacoin'
-    })
-    .then(data => {
-      res.send(data)
-    })
-    .catch(console.error)
+  cache.get(TICKER_KEY, (err, val) => {
+    if (!err){
+      if (val === undefined){
+        cmc
+        .getTicker({
+          currency: 'siacoin'
+        })
+        .then(data => {
+          cache.set(TICKER_KEY, data)
+          res.send(data)
+        })
+        .catch(console.error)
+      } else {
+        //cache hit
+        res.send(val)
+      }
+    } else {
+      console.log('Error in caching CMC infrastructure.  This needs to be investigated. Making the API call as a fallback')
+      cmc
+      .getTicker({
+        currency: 'siacoin'
+      })
+      .then(data => {
+        cache.set(TICKER_KEY, data)
+        res.send(data)
+      })
+      .catch(console.error)
+    }
+  })
+
 })
 
 module.exports = router
